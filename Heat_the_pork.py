1f3b9e26daf900b000c6d4f151549fd7cfe26dc3
user <bua@bua.com>
2021-04-15 16:41:13 +0300

    надо заканчивать с переносом и идти дальше

--- Heat_the_pork.py
+++ Heat_the_pork.py
@@ -9,17 +9,20 @@ def second_window_open():
                     def seventh_window1_open():
                         def eight_window_open():
                             def next_line_func(list):
+                                sum = 0
                                 for i in range(len(list)):
-                                    sum = 0
+
                                     for x in list:
-                                        sum += len(x)
-                                        i += 1
-                                        if sum > 30:
-                                            list[i-1] = x + '\n'
-                                            print(list)
-                                            return item_list_to_str()
+                                        while sum <= 30:
+                                            sum += len(x)
+                                            i += 1
+                                            item_list_length_check(sum, list, i, x)
 
-
+                            def item_list_length_check(sum, list, i, x):
+                                if sum > 30:
+                                    list[i] = x + '\n'
+                                    print(list)
+                                    return item_list_to_str()
 
 
 
@@ -31,8 +34,6 @@ def second_window_open():
 
                                 return item_list_str
 
-
-
                             eight_window = Tk()
                             eight_window.geometry("800x680+700+300")
                             eight_window['bg'] = '#C4C4C4'
@@ -42,10 +43,7 @@ def second_window_open():
                             item_list = inventory_str.split(',')
                             item_list = [x.strip(' ') for x in item_list]
 
-
-                                        # придумать как в середину строки  вписать "\n", если она больше 50ти символов
-
-
+                            # придумать как в середину строки  вписать "\n", если она больше 50ти символов
 
                             window_label_8 = Label(eight_window,
                                                    bg='#C4C4C4',
@@ -59,14 +57,13 @@ def second_window_open():
                             window_label_8.grid(column=0, row=0,
                                                 pady="85")
                             button_ok = Button(eight_window, text="OK",
-                                                width="12", height="1", font=("Arial Bold", 18))
+                                               width="12", height="1", font=("Arial Bold", 18))
                             button_ok.place(x='65', y='630')
                             eight_window.mainloop()
 
                         def seventh_window1_push_yes():
                             seventh_window1.destroy()
                             eight_window_open()
-
 
                         seventh_window1 = Tk()
                         seventh_window1.geometry("600x680+700+300")
@@ -88,13 +85,12 @@ def second_window_open():
                                             pady="85")
                         inventory_raw = StringVar()
                         entry_seventh_win = Entry(seventh_window1, width=60, textvariable=inventory_raw)
-                        entry_seventh_win.place (x='60', y='550')
+                        entry_seventh_win.place(x='60', y='550')
                         button_yes = Button(seventh_window1, text="ДА",
                                             width="12", height="1", font=("Arial Bold", 18),
                                             command=seventh_window1_push_yes)
                         button_yes.place(x='65', y='630')
                         seventh_window1.mainloop()
-
 
                     def seventh_window2_open():
                         def seventh_win2_push_no():
@@ -114,21 +110,19 @@ def second_window_open():
                         window_label_8.grid(column=0, row=0,
                                             pady="85")
                         button_no = Button(seventh_window2, text="НЕТ",
-                                           width="12", height="1", font=("Arial Bold", 18),command=seventh_win2_push_no)
+                                           width="12", height="1", font=("Arial Bold", 18),
+                                           command=seventh_win2_push_no)
                         button_no.place(x='310', y='630')
 
                         seventh_window2.mainloop()
-
 
                     def sixth_win_push_yes():
                         sixth_window.destroy()
                         seventh_window1_open()
 
-
                     def sixth_win_push_no():
                         sixth_window.destroy()
                         seventh_window2_open()
-
 
                     sixth_window = Tk()
                     sixth_window.geometry("500x467+700+300")
@@ -137,9 +131,9 @@ def second_window_open():
                     window_label_6 = Label(sixth_window,
                                            bg='#C4C4C4',
                                            text="**Впереди ты видишь стол с" "\nедой, но не можешь"
-                                           "\nрассмотреть,что именно за"
-                                           "\nеда на нем "
-                                           "\n\nПодойти поближе?",font = ("Arial Bold", 20), justify=LEFT,
+                                                "\nрассмотреть,что именно за"
+                                                "\nеда на нем "
+                                                "\n\nПодойти поближе?", font=("Arial Bold", 20), justify=LEFT,
                                            padx='60')
                     window_label_6.grid(column=0, row=0,
                                         pady="85")
@@ -184,8 +178,8 @@ def second_window_open():
             fourth_window.title(" ")
             window_label_4 = Label(fourth_window, bg='#C4C4C4',
                                    text="Достойное имя для\nприключенца!\n \n(моего кота звали так же)",
-            font = ("Arial Bold", 20),
-                   justify = LEFT, padx = '60')
+                                   font=("Arial Bold", 20),
+                                   justify=LEFT, padx='60')
             window_label_4.grid(column=0, row=0, pady="85")
             button_ok_fourth_win = Button(fourth_window,
                                           text="А?..", width="12", height="1", font=("Arial Bold", 18),
@@ -193,7 +187,6 @@ def second_window_open():
                                           command=fourth_button_clicked)
             button_ok_fourth_win.place(x='150', y='370')
             fourth_window.mainloop()
-
 
         def third_window_clicked():
             third_window.destroy()
@@ -236,8 +229,8 @@ def second_window_open():
                            text="Отлично! \n"
                                 "\nТогда тебе как никому в\nэтом заведении понятно:\n"
                                 "\nСытый пуник -- доаольный\nпутник!",
-    font = ("Arial Bold", 20), justify = LEFT,
-                                         padx = '60')
+                           font=("Arial Bold", 20), justify=LEFT,
+                           padx='60')
     window_label_2.grid(column=0, row=0, pady="85")
     second_window.mainloop()
 
@@ -262,8 +255,8 @@ button_no.place(x='310', y='270')
 first_window.title("Приветствую!")
 window_label_1 = Label(first_window, bg='#C4C4C4',
                        text="Приветствую пупник! \n" " \nВерно ты устал после долгой \nи опасной дороги?",
-font = ("Arial Bold", 20), justify = LEFT,
-                                     padx = '60')
+                       font=("Arial Bold", 20), justify=LEFT,
+                       padx='60')
 window_label_1.grid(column=0, row=0, pady="85")
 
 first_window.mainloop()
